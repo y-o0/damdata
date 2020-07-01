61ae051dbba086ef68d39c2aa2e9b2138c238071
root <root@ip-172-31-13-224.ap-northeast-1.compute.internal>
2020-07-02 00:20:37 +0900

    20200702-002037

--- fetcher.rb
+++ fetcher.rb
@@ -6,19 +6,6 @@ class Fetcher
   URL = 'http://www.cjh.com.cn/sqindex.html'
 
   class << self
-    using Module.new {
-      refine Time do
-        # magic
-        def Time.now_in_jst
-          prev_tz = ENV['TZ']
-          ENV['TZ'] = 'JST'
-          self.now.localtime.tap do
-            ENV['TZ'] = prev_tz
-          end
-        end
-      end
-    }
-
     # @return [String] filepath
     def save
       url = URI.parse(Fetcher::URL)
@@ -26,7 +13,7 @@ class Fetcher
       res = Net::HTTP.start(url.host, url.port) do |http|
         http.request(req)
       end
-      File.expand_path("../snapshots/#{Time.now_in_jst.strftime('%Y-%m-%d-%H%M%S')}.html", __FILE__).to_s.tap do |file_path|
+      File.expand_path("../snapshots/#{Time.now.strftime('%Y-%m-%d-%H%M%S')}.html", __FILE__).to_s.tap do |file_path|
         File.open(file_path, "w") do |writer|
           writer << res.body.gsub('assetscjsw/plugin/jquery-1.11.3.min.js', 'https://code.jquery.com/jquery-1.12.4.min.js')
         end
