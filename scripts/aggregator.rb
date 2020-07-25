faf2ee3e84b043d51bb034f00b1bd6cc1771fd88
y-o0 <55176390+y-o0@users.noreply.github.com>
2020-07-25 17:17:35 +0900

    spread the date range  of the data

--- scripts/aggregator.rb
+++ scripts/aggregator.rb
@@ -5,7 +5,7 @@ class Aggregator
   class << self
     # @return [String] json
     def convert
-      Dir.glob(File.expand_path('../../jsons/*.json', __FILE__)).sort.reverse[0..21 * 24].reduce(Hash.new) do |acc, cur|
+      Dir.glob(File.expand_path('../../jsons/*.json', __FILE__)).sort.reverse[0..30 * 24].reduce(Hash.new) do |acc, cur|
         JSON.load(File.read(cur)).each do |r|
           acc[r['stnm']] ||= Hash.new
           acc[r['stnm']][r['tm']] = { z: r['z'].to_f, q: r['q'].to_i, oq: r['oq'].to_i }
