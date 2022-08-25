5f11a61068b91f2a12aa0a8c2018a2c69cfde8c2
y-o0 <55176390+y-o0@users.noreply.github.com>
2022-08-26 03:06:26 +0900

    aggregate only this year

--- scripts/aggregator.rb
+++ scripts/aggregator.rb
@@ -1,11 +1,12 @@
 require 'json'
+require 'date'
 
 # @abstract concat jsons and convert them to a well-structured json
 class Aggregator
   class << self
     # @return [String] json
     def convert
-      Dir.glob(File.expand_path('../../jsons/*.json', __FILE__)).sort.reverse[0..30 * 24].reduce(Hash.new) do |acc, cur|
+      Dir.glob(File.expand_path("../../jsons/#{Date.today.year}*.json", __FILE__)).sort.reverse[0..30 * 24].reduce(Hash.new) do |acc, cur|
         JSON.load(File.read(cur)).each do |r|
           acc[r['stnm']] ||= Hash.new
           acc[r['stnm']][r['tm']] = { z: r['z'].to_f, q: r['q'].to_i, oq: r['oq'].to_i }
